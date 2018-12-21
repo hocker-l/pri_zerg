4a08572e7149695497048ab8640c8c83021a34e1
lsp <942887391@qq,com>
2018-12-21 17:27:10 +0800

    更新

---
+++ application/api/controller/v1/Banner.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lsp
+ * Date: 2018/12/21
+ * Time: 17:18
+ */
+
+namespace app\api\controller\v1;
+
+
+class Banner
+{
+    function banner(){
+        echo "banner";
+    }
+}
---
+++ application/api/controller/v2/Banner.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lsp
+ * Date: 2018/12/21
+ * Time: 17:21
+ */
+
+namespace app\api\controller\v2;
+
+
+class Banner
+{
+    function banner(){
+        echo "baner2";
+    }
+
+}
